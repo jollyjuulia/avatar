61d0aa16dc16c5f4487ce10c0a50ccfa0f59bf85
jollyjuulia <ninjacrafter1009@gmail.com>
2025-03-01 23:16:15 -0500

    nj

--- makeup/script.js
+++ makeup/script.js
@@ -1,24 +1,22 @@
-// Get the button elements and the scroll container
+
 const scrollLeftButton = document.getElementById('scrollLeftButton');
 const scrollRightButton = document.getElementById('scrollRightButton');
 const scrollContainer = document.querySelector('.grid-container');
 
-// Function to scroll the container smoothly by a specific amount
+
 function smoothScroll(amount) {
-  // Use the scrollLeft property with a smooth scroll behavior
+ 
   scrollContainer.scrollBy({
     left: amount,
-    behavior: 'smooth', // This ensures smooth scrolling
+    behavior: 'smooth', 
   });
 }
 
-// Scroll left on button click
 scrollLeftButton.addEventListener('click', function () {
-  smoothScroll(-2900); // Scroll left by 200px
+  smoothScroll(-2900); 
 });
 
-// Scroll right on button click
 scrollRightButton.addEventListener('click', function () {
-  smoothScroll(2900); // Scroll right by 200px
+  smoothScroll(2900); 
 });
 
